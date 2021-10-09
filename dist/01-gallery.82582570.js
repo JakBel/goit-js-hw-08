00a46bb44b83ce5c6c76c7d1751d6ea28dd4ac87
JakBel <73158151+JakBel@users.noreply.github.com>
2021-10-09 17:18:41 +0200

    6

--- dist/01-gallery.82582570.js
+++ dist/01-gallery.82582570.js
@@ -2056,7 +2056,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63024" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49777" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
